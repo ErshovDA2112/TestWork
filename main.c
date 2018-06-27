93d340aaa61f4ed885c493ec1ae3f46f69d54d75
Dmitriy <superbrat@mail.ru>
2018-06-27 16:19:39 +0300

    Изменение последнее

--- main.c
+++ main.c
@@ -2,13 +2,15 @@
 #include < unistd.h >
 
 double t=0.1;
-double g=6.9;
+int y=777777777777777777777;
+double g=6.9
 
 int main (void)
-{
-int k=0;;
-printf (“Hello word!!!\n”);;
-sleep (5);;
+{///
+789879645
+int k=0;
+printf (“Hello word!!!\n”);
+sleep (10);
 
 return k;;
 }
