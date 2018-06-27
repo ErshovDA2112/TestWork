7e4ab5d2c9cfac65690062934ffe39e90ea3afcb
Dmitriy <superbrat@mail.ru>
2018-06-27 15:21:45 +0300

    второе изменения
    # Conflicts:
    #	main.c

--- main.c
+++ main.c
@@ -1,16 +1,11 @@
 #include < stdio.h >
 #include < unistd.h >
 
-double t=0.1;
-int y=777777777777777777777;
-double g=6.9
-
 int main (void)
-{///
-789879645
+{
 int k=0;
 printf (“Hello word!!!\n”);
-sleep (10);
+sleep (5);
 
-return k;;
+return k;
 }
